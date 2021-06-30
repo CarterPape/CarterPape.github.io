6dc233599ce18dc20666d399529617cbc720a618
Carter Pape <github.com@carterpape.com>
2021-06-29 21:42:06 -0600

    balances all h tags

--- webpack/main.js
+++ webpack/main.js
@@ -6,3 +6,22 @@
 import balanceText from 'balance-text';
 
 balanceText();
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    let relevant_header_tags = ["h2", "h3", "h4", "h5", "h6"];
+    let all_headers = [];
+    
+    relevant_header_tags.forEach(function(header_tag) {
+        all_headers.push(
+            ...Array.from(document.getElementsByTagName(header_tag))
+        );
+    });
+    
+    all_headers.forEach(function(element) {
+        element.classList.add("balance-text")
+    });
+    
+    balanceText();
+})
+
+import {} from 'mathjax/es5/tex-chtml.js';
