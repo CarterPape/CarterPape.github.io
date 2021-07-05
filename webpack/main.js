a1359eafdc4671e2f893f6ad362a7baf4f3207da
Carter Pape <github.com@carterpape.com>
2021-07-05 14:59:36 -0600

    moves nav JS into main JS file

--- webpack/main.js
+++ webpack/main.js
@@ -7,7 +7,20 @@ import balanceText from 'balance-text';
 
 balanceText();
 
-document.addEventListener('DOMContentLoaded', function(event) {
+function toggleNavList() {
+    var navList = document.getElementById("sitewide-nav");
+    navList.style.display = (navList.style.display == "flex" ? "" : "flex");
+    
+    var navHamburger = document.getElementById("sitewide-nav-hamburger");
+    navHamburger.style.display = (navHamburger.style.display == "none" ? "" : "none");
+    
+    var navExit = document.getElementById("sitewide-nav-exit-button");
+    navExit.style.display = (navExit.style.display == "none" ? "" : "none");
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById("sitewide-nav-button").onclick = toggleNavList;
+    
     let relevant_header_tags = ["h2", "h3", "h4", "h5", "h6"];
     let all_headers = [];
     
